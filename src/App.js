664c97ce1a05f7489a48c754b95117756b0a37ad
agan-k <koranagan@gmail.com>
2023-02-06 23:30:49 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,5 @@
+export default function App() {
+  return (
+    <h1>Dominique Carre</h1>
+  )
+}
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import {createRoot} from 'react-dom/client'
+import {ThemeProvider} from 'styled-components'
+import App from './App'
+import theme from './theme'
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>
+);
+
+if (module.hot) {
+  module.hot.accept(function () {
+    window.location.reload();
+  });
+}
