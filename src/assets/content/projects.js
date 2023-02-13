95208c9b2ef72b57de3cfff5e1200d6475923818
agan-k <koranagan@gmail.com>
2023-02-13 17:19:19 -0500

    add bio and projects content

---
+++ src/assets/content/projects.js
@@ -0,0 +1,43 @@
+/* eslint-disable max-len */
+import fusionProject from 'url:../images/fusion-project.jpg';
+import acousticCombo from 'url:../images/acoustic-combo.jpg';
+import tomBabylone from 'url:../images/tom-babylone.jpg';
+export const projects = [
+  {
+    name: 'Fusion Project',
+    desc: `
+    A new jazz groove with a French touch! Dominique Carré's new project immerses us in a whole new world. Dominique combines Gypsy jazz with urban rock and funk frenzy. The standards revisit the incredible Django Reinhardt and borrows from the guitarist's compositions.
+
+    Dominique's quartet of jazz musicians find their inspiration from Django as well as from Dominique's own original compositions. Their music has a sound of vital energy; a contagious wildness with a powerful groove.
+    "All of the inspirations for the compositions and arrangements of the record NYCarré come from Django: without whom this record wouldn't exist." -- DC
+    `,
+    img: fusionProject,
+    personel: 'Dominique Carré guitar, Guillaume Julien guitar, samples, Pierrick Biffot bass, Mickaël Jamier drums.',
+  },
+  {
+    name: 'Acoustic Combo',
+    desc: `
+    Après plusieurs albums nomades reconnus par la critique entre tradition manouche (Après la Pluie- 2008, Rendez Vous- 2010, Cancale sessions-2018) et fusion (Le Bourdon des Abeilles-1998, NYCarré 2015, L’électric Combo 2019), Dominique reviens en toute simplicité vers un nouveau projet à la mode Django.
+
+    Le nouveau combo acoustique de Dominique s’articule autour des thèmes chers à Django Reinhardt: My serenade, Chicago, Fine and dandy, Anouman,...mais aussi Boogie Stop Schuffle de Charlie Mingus La mélodie perdue chanté par Bamboula Ferret, Mademoiselle d’Henri Salvador, L.O.V.E de Nat king Cole...
+    `,
+    img: acousticCombo,
+    personel: `
+    Dominique Carré est à la guitare lead et chant accompagné de
+    Morgan Bonnot à la pompe manouche et Julien « dédé » Pinel à la
+    Contrebasse
+    `,
+  },
+  {
+    name: 'Tom Babylone',
+    desc: `
+    L' ascenseur de 19h43  en provenance de Babylone est arrivée et c'est Tom qui est descendu sur le quai. Son bagage en main il choisit le vent comme ami, puis la mécanique des fluides, et s en va rejoindre le soleil et la pluie. L histoire ne fait que commencer et la musique se pare d étoffes au creux des pierres au fond des étoiles.
+    Le Tom Babylone quartet vous en fait voir de toutes les couleurs pour un voyage en Tziganie. De la Roumanie  en passant par Rungis ou Salamanque, direction le Rajasthan, Tom  est là avec vous, pour vous mais surtout ailleurs. N' ayez crainte  l'aventure et l'improvisation  sont au présent. 
+    Le voyage ne fait que commencer!
+    `,
+    img: tomBabylone,
+    personel: `
+    Benoit Josse - Violon, Dominique Carré - Guitare, Morgan Bonnot - Guitare, Guillaume Jurkiewicz - Contrebasse
+    `,
+  },
+];
