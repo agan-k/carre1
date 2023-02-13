c5fc084f3b45ed6ff428a92875a9e39f8799d49e
agan-k <koranagan@gmail.com>
2023-02-13 17:27:19 -0500

    add statement to home page and bio pic

--- src/pages/bio/Bio.js
+++ src/pages/bio/Bio.js
@@ -1,10 +1,11 @@
 import {bioFrench} from "~src/assets/content/bio";
 import {MultiParagraphText} from "~src/components/multi-paragraph-text";
-
+import bioPic from '~src/assets/images/carre-bio.jpg';
 export default function Bio() {
   return(
     <>
       <h1>Bio</h1>
+      <img src={bioPic} width="100" />
       <MultiParagraphText text={bioFrench} />
     </>
   );
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,3 +1,10 @@
+import {statementFrench} from "~src/assets/content/statement";
+
 export default function Home() {
-  return <h1>Dominique Carre home ...</h1>;
+  return(
+    <>
+      <h1>Dominique Carre home</h1>
+      <p>{statementFrench}</p>
+    </>
+  );
 };
