a0a68bf30684edef78947f2ae83dc2b072124186
agan-k <koranagan@gmail.com>
2023-02-14 11:28:29 -0500

    add contact/Press

---
+++ src/assets/content/contactPress.js
@@ -0,0 +1,62 @@
+/* eslint-disable max-len */
+export const contact = [
+  {
+    booking:
+    {
+      email: 'prog@dominiquecarre.fr',
+      phone: 'Emmanuelle +33 (0)6 07 99 80 38',
+    },
+    info: {
+      email: '3et4prod@dominiquecarre.fr',
+    },
+  },
+];
+
+export const pressFrench = [
+  {
+    quote: 'Dominique Carré dessine un univers singulier qui suppose la mise en présence de pôles totalement opposés, la frénésie urbaine (le terme est faible) new-yorkaise et le gypsy-swing.',
+    source: 'Acoustic Mag',
+  },
+  {
+    quote: 'Le guitariste triture, destructure la littérature reinhardtienne, jonglant entre les sonorités et les influences. on pense à Django, Benson, Metheny, Scofield...',
+    source: 'Jazz News',
+  },
+  {
+    quote: 'De belles prouesses guitaristiques et des idées intéressantes.',
+    source: 'Jazz Mag',
+  },
+  {
+    quote: 'Les morceaux prennent un aspect plus contemporain.',
+    source: 'Uzu Melodiés',
+  },
+];
+export const pressEnglish = [
+  {
+    quaote: 'Dominique Carré creates a new universe which implies the presence of polar opposites: New York style urban frenzy, and gypsy-swing.',
+    source: 'Acoustic Mag',
+  },
+  {
+    quote: `The guitarist manipulates Django's style of gypsy jazz, juggling between sounds and influences. It brings to mind Django, Benson, Metheny, Scofield...`,
+    source: 'Jazz News',
+  },
+  {
+    quote: 'Beautiful guitarist prowess and interesting ideas.',
+    source: 'Jazz Mag',
+  },
+  {
+    quote: 'The pieces take on an aspect of contemporary melodies."',
+    source: 'Uzu Melodiés',
+  },
+];
+
+export const downloads = [
+  {
+    name: 'Photos',
+    file: '',
+  },
+  {
+    name: 'Fiche Technique',
+    file: '',
+  },
+];
+
