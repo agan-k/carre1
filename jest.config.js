d3394502a3b2bb352e5132080b437ec6d79dcd5f
agan-k <koranagan@gmail.com>
2023-02-26 08:01:03 -0500

    add jest-transfom-stub, add moudle mapper to jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
+/* eslint-disable max-len */
 module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.{js,jsx}'],
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+    'jest-transform-stub',
+  },
 };
