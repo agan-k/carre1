a781178925901a19527d628b2232c16b67814bcb
agan-k <koranagan@gmail.com>
2023-03-01 11:05:58 -0500

    add validation for empty fields

--- src/pages/tour-dates/TourDates.js
+++ src/pages/tour-dates/TourDates.js
@@ -6,10 +6,22 @@ export default function TourDates() {
     //Prismic has no required fields (https://prismic.io/blog/required-fields)
     //so using index for key prop here:
     <div key={item.data.timestamp + index}>
-      <PrismicRichText field={item.data?.venue} />
-      <p>{asDate(item.data.timestamp).toLocaleString()}</p>
-      <PrismicRichText field={item.data?.date} />
-      <PrismicRichText field={item.data?.description} />
+      {
+        item.data?.venue.length !== 0 ? (
+          <h3>{item.data?.venue[0].text}</h3>
+        ) : ''
+      }
+      {
+        item.data?.timestamp !== null ? (
+          <p>{asDate(item.data.timestamp).toLocaleString()}</p>
+        ) : ''
+      }
+      {
+        item.data?.description !== 0 ? (
+          <PrismicRichText field={item.data?.description} />
+        ) : ''
+      }
+      <hr></hr>
     </div>
   );
   return showCards;
