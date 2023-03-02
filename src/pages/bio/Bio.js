e35803d1c14aefe67ad60aad589a91279a2c4169
agan-k <koranagan@gmail.com>
2023-03-02 11:41:05 -0500

    replace local bio data with prismic

--- src/pages/bio/Bio.js
+++ src/pages/bio/Bio.js
@@ -1,10 +1,12 @@
 import {useOutletContext} from "react-router-dom";
-import {bioFrench, bioEnglish} from "../../assets/content/bio";
-import {MultiParagraphText} from "../../shared";
+import {useSinglePrismicDocument, PrismicRichText} from "@prismicio/react";
 import bioPic from '../../assets/images/carre-bio.jpg';
 
 export default function Bio() {
   const [language] = useOutletContext();
+  const [bioObj] = useSinglePrismicDocument('bio');
+  const bioEnglish = bioObj?.data.bio_english;
+  const bioFrench = bioObj?.data.bio_french;
   const bio = language === 'french' ?
     bioFrench : bioEnglish;
 
@@ -12,7 +14,10 @@ export default function Bio() {
     <>
       <h1>Bio</h1>
       <img src={bioPic} width="100" />
-      <MultiParagraphText text={bio} />
+      {
+        bio?.length !== 0 ? 
+          <PrismicRichText field={bio} /> : ''
+      }
     </>
   );
 };
