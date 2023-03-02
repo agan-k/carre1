8963f0cc66d26b412ea77b4fb40ce1d13d951f69
agan-k <koranagan@gmail.com>
2023-03-02 16:06:47 -0500

    fix key prop

--- src/pages/tour-dates/TourDates.js
+++ src/pages/tour-dates/TourDates.js
@@ -2,10 +2,8 @@ import {useAllPrismicDocumentsByType, PrismicRichText} from "@prismicio/react";
 import {asDate} from "@prismicio/helpers";
 export default function TourDates() {
   const [shows] = useAllPrismicDocumentsByType('shows');
-  const showCards = shows?.map((item, index) => 
-    //Prismic has no required fields (https://prismic.io/blog/required-fields)
-    //so using index for key prop here:
-    <div key={item.data.timestamp + index}>
+  const showCards = shows?.map((item) =>
+    <div key={item.data.id}>
       {
         item.data?.venue.length !== 0 ? (
           <h3>{item.data?.venue[0].text}</h3>
