5f2ac0f5b119acd3d702b48ac913fb34e828d946
agan-k <koranagan@gmail.com>
2023-03-02 11:34:54 -0500

    validate for prismic Home type empty fields

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,15 +4,18 @@ import {useSinglePrismicDocument, PrismicRichText} from "@prismicio/react";
 export default function Home() {
   const [language] = useOutletContext();
   const [homeObj] = useSinglePrismicDocument('home');
-  //TODO: validate both for empty field
   const statementEnglish = homeObj?.data.statement_english;
   const statementFrench = homeObj?.data.statement_french;
   const statement = language === 'french' ?
     statementFrench : statementEnglish;
+
   return(
     <>
       <h1>Dominique Carre Home</h1>
-      <PrismicRichText field={statement} />
+      {
+        statement?.length !== 0 ? 
+          <PrismicRichText field={statement} /> : ''
+      }
     </>
   );
 };
