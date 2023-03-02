3422fdf0aa5c25051d0f5375393335240059e60a
agan-k <koranagan@gmail.com>
2023-03-01 23:56:18 -0500

    replace home data with prismic

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,15 +1,18 @@
 import {useOutletContext} from "react-router-dom";
-import {statementFrench, statementEnglish} from 
-  "../../assets/content/statement";
+import {useSinglePrismicDocument, PrismicRichText} from "@prismicio/react";
 
 export default function Home() {
   const [language] = useOutletContext();
+  const [homeObj] = useSinglePrismicDocument('home');
+  //TODO: validate both for empty field
+  const statementEnglish = homeObj?.data.statement_english;
+  const statementFrench = homeObj?.data.statement_french;
   const statement = language === 'french' ?
     statementFrench : statementEnglish;
   return(
     <>
-      <h1>Dominique Carre home</h1>
-      <p>{statement}</p>
+      <h1>Dominique Carre Home</h1>
+      <PrismicRichText field={statement} />
     </>
   );
 };
