4d0dd633c0693707da6ce058c26456c4d75b79cb
agan-k <koranagan@gmail.com>
2023-03-02 15:57:07 -0500

    replace album data with CMS

--- src/pages/music/Music.js
+++ src/pages/music/Music.js
@@ -1,22 +1,37 @@
-import {MultiLineText} from "../../shared";
-import albums from "../../assets/content/albums";
+import {useOutletContext} from "react-router-dom";
+import {useAllPrismicDocumentsByType, PrismicRichText} from "@prismicio/react";
+
 export default function Music() {
-  return(
-    <>
-      {albums.map(item => 
-        <div key={item.title}>
-          <img src={item.img ? item.img : ''} width="100" />
-          <MultiLineText text={item.personel} />
-          {item.video ? 
-            <iframe 
-              width="300" 
-              height="200" 
-              src={item.video} 
-              title="YouTube video player"
-            /> : null
-          }
-        </div>
-      )}
-    </>
-  );
+  const [language] = useOutletContext();
+  const [albumsData] = useAllPrismicDocumentsByType('albums');
+
+  const albums = albumsData?.map((item) => {
+    const title = item.data.album_title;
+    const personnel = item.data.album_personnel;
+    const descriptionFrench = 
+      item.data.french_album_description_french || [];
+    const descriptionEnglish = 
+      item.data.english_album_description_english || [];
+    const description = language === 'french' ?
+      descriptionFrench : descriptionEnglish;
+
+    return(
+      <div key={item.id}>
+        <img src={item.data.album_image.url} width="200"/>
+        <PrismicRichText field={ title.length !== 0 ? title : ''} />
+        <PrismicRichText field={description.length !== 0 ? description : ''}/>
+        <PrismicRichText field={personnel.length !== 0 ? personnel : ''}/>
+        {item.data.video.url !== null ? 
+          <iframe 
+            width="300" 
+            height="200" 
+            src={item.data.video.url} 
+            title="YouTube video player"
+          /> : ''
+        }
+        <hr></hr>
+      </div>
+    );
+  });
+  return albums;
 };
