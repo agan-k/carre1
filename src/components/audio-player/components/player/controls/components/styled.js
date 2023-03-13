7b77034cfeefe10aadf0f12b11276c2606e519bc
agan-k <koranagan@gmail.com>
2023-03-13 18:47:32 -0400

    increase audio control button size

--- src/components/audio-player/components/player/controls/components/styled.js
+++ src/components/audio-player/components/player/controls/components/styled.js
@@ -3,8 +3,8 @@ import {space, layout, color, flexbox} from 'styled-system';
 
 const ControlsButton = styled('button')(
   {
-    width: '30px',
-    height: '30px',
+    width: '40px',
+    height: '40px',
     border: 'none',
     backgroundColor: 'white',
   },
