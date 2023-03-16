c07589ca5a57cc2e9dadcd2c83328f479bc182cc
agan-k <koranagan@gmail.com>
2023-03-16 14:44:45 -0400

    reorganize Tour dates page

--- src/pages/tour-dates/TourDates.js
+++ src/pages/tour-dates/TourDates.js
@@ -1,25 +1,28 @@
 import {useAllPrismicDocumentsByType, PrismicRichText} from "@prismicio/react";
 import {asDate} from "@prismicio/helpers";
+import {Box} from '../../shared';
+
 export default function TourDates() {
   const [shows] = useAllPrismicDocumentsByType('shows');
   const calendarCards = shows?.map((item) => {
-    const hasVenue = Boolean(item.data?.venue.length !== 0);
-    const hasTime = Boolean(item.data?.timestamp !== null);
-    const hasDescription = Boolean(item.data?.description !== 0);
+    const venue = item.data.venue;
+    const hasVenue = Boolean(venue.length !== 0);
+    const hasTime = Boolean(item.data.timestamp !== null);
+    const hasDescription = Boolean(item.data.description !== 0);
     return (
-      <div key={item.id}>
-        {hasVenue ? <h3>{item.data?.venue[0].text}</h3> : ''}
+      <Box key={item.id} p={4} borderBottom={'1px solid gray'}>
         {hasTime ? <p>{asDate(item.data.timestamp).toLocaleString()}</p> : ''}
+        {hasVenue ? <h3>{venue[0].text}</h3> : ''}
         {hasDescription ? 
           <PrismicRichText field={item.data?.description} /> : ''}
-        <hr></hr>
-      </div>
+      </Box>
     );
   });
+
   return (
-    <>
+    <Box>
       <h1>Tour Dates</h1>
       {calendarCards}
-    </>
+    </Box>
   );
 };
