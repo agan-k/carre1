aa17ad7956e308e3a5259a6f8491539e971d393c
agan-k <koranagan@gmail.com>
2023-03-16 21:45:26 -0400

    fix margin under unordered list in Calendar Card

--- src/pages/tour-dates/TourDates.js
+++ src/pages/tour-dates/TourDates.js
@@ -1,6 +1,7 @@
 import {useAllPrismicDocumentsByType, PrismicRichText} from "@prismicio/react";
 import {asDate} from "@prismicio/helpers";
 import {Box} from '../../shared';
+import {CalendarCardWrapper} from "./styled";
 
 export default function TourDates() {
   const [shows] = useAllPrismicDocumentsByType('shows');
@@ -10,12 +11,12 @@ export default function TourDates() {
     const hasTime = Boolean(item.data.timestamp !== null);
     const hasDescription = Boolean(item.data.description !== 0);
     return (
-      <Box key={item.id} borderBottom={'1px solid gray'}>
+      <CalendarCardWrapper key={item.id}>
         {hasTime ? <p>{asDate(item.data.timestamp).toLocaleString()}</p> : ''}
         {hasVenue ? <h3>{venue[0].text}</h3> : ''}
         {hasDescription ? 
           <PrismicRichText field={item.data?.description} /> : ''}
-      </Box>
+      </CalendarCardWrapper>
     );
   });
 
---
+++ src/pages/tour-dates/styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import {Box} from '../../shared';
+
+export const CalendarCardWrapper = styled(Box)(
+  {
+    borderBottom: '1px solid gray',
+    ul: {
+      marginBottom: '16px',
+    },
+  }
+);
