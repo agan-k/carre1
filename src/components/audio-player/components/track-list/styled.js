cea0b24de81130e999fcf9aa3f7c715dde925d84
agan-k <koranagan@gmail.com>
2023-03-22 22:01:51 -0400

    add background color to track list for iOS chrome

--- src/components/audio-player/components/track-list/styled.js
+++ src/components/audio-player/components/track-list/styled.js
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 import {space, layout, color} from 'styled-system';
+import {theme} from '../../../../theme';
 
 export const TracklistWrapper = styled.div(
   {
     height: props => (props.isOpenTrackList ? 'auto' : '0'),
     overflow: 'hidden',
     width: '100%',
+    backgroundColor: theme.colors.background,
   },
   space,
   layout,
