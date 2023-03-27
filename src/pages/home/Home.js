f44eb2979771d010a7b8e4e08fa7a8397a10387b
agan-k <koranagan@gmail.com>
2023-03-27 14:38:02 -0400

    adjust responsive image size for Home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -11,15 +11,16 @@ export default function Home() {
   const imageURL = homeData?.data.home_image.url;
   return(
     <PageWrapper display={['block', 'flex', 'flex', 'flex']}>
-      <Box flex={'1 1 50%'}>
+      <Box
+        flex={['unset', '1 1 20%', '1 1 30%', 'unset']}>
         <img src={imageURL} width={'100%'}/>
       </Box>
       <Box 
         flex={'1 1 50%'}
         p={[
           theme.space[1], 
-          theme.space[5], 
-          theme.space[5], 
+          theme.space[4], 
+          theme.space[4], 
           theme.space[6]]}>
         <FeaturedPress />
         <News />
--- src/pages/styled.js
+++ src/pages/styled.js
@@ -1,17 +1,12 @@
 import styled from 'styled-components';
 import {HeadingSmall, Box} from '../shared';
 import {theme} from '../theme';
-import mediaQuery from '../utils/mediaQuery';
 
 export const PageWrapper = styled(Box)(
   {
     paddingLeft: theme.space[3],
     paddingRight: theme.space[3],
-  },
-  mediaQuery(
-    {
-    }
-  )
+  }
 );
 export const PageTitle = styled(HeadingSmall)(
   {
