22d34ef0db06235aeb7741d1ae557c96106476f0
agan-k <koranagan@gmail.com>
2023-03-30 15:58:07 -0400

    remove unused assets

--- src/assets/content/statement.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable max-len */
-export const statementFrench = `
-  "La musique a été tout de suite pour moi un moyen de partage et d' illumination du présent. À l âge de 12 ans, j ai assisté à un concert d'un guitariste gitan! J 'ai été transporté par la fougue et la joie du musicien. Sa musique coulait à flot dans un élan naturel..Le fait qu'il joue sans connaître la musique, les notes" m'a donné envie d'apprendre par cette voie: La tradition orale. Je me suis formé comme ça en autodidacte, au gré des rencontres et en écoutant des disques..Le rock et le funk ont étés mes premiers amours puis le jazz rock à la mode dans les annees 80, puis plus rien. ..Puis enfin Django! Une révélation sur le tard mais qui ne me quittera plus jamais, c'est certain." -- DC
-`;
-
-export const statementEnglish = `
-"Right away, music, for me, was a medium to share and illuminate the present, to evoke emotions, and to immediately transmit emotions. At twelve years old, I helped out at a gypsy guitarists concert! I was tranported by the eccentricity of the musician, his joy. Mediterranean rhythms seduced me and the fact that he played without "knowing the music, the notes" inspired me to learn through the oral tradition. I was trained as a self-taught person, aided by encounters with other musicians and by listening to records. The rock influence was the style of the day, then the jazz a little later, and finally Django Reinhardt. A late revelation but one which will never leave me." -- DC
-`;
-
