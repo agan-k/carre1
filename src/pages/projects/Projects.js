ceb42077b32b2c97fa81d65ce20cebea324ca843
agan-k <koranagan@gmail.com>
2023-04-13 23:38:21 -0400

    render new lineup field

--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -1,6 +1,6 @@
 import {useOutletContext, useLocation} from "react-router-dom";
 import {useAllPrismicDocumentsByType, PrismicRichText} from "@prismicio/react";
-import {Box, Text} from "../../shared";
+import {Box, Text, Flex} from "../../shared";
 import {routes} from '../../router';
 import {PageTitle, PageWrapper, Loading} from "../styled";
 import {ProjectWrapper} from "./styled";
@@ -14,11 +14,22 @@ export default function Projects() {
   const currentPage = routes.find(item =>  location.pathname === item.path);
   const pageTitleInActiveLanguage = 
       currentPage.element.props.name[`${language}`];
-      
+  
   const projects = projectsData?.map((item) => {
+    const lineup = item.data.lineup.map((item) =>
+      <Flex key={item.name} flexWrap={'wrap'}>
+        <Text fontWeight={'bold'}>
+          {item.name}
+        </Text>
+        {item.instrument ?
+          <Text fontStyle={'italic'}>
+            &nbsp;-&nbsp;{item.instrument.toLowerCase()}
+          </Text> : ''
+        }
+      </Flex>
+    );
     const imageURL = item.data.project_image.url;
     const name = item.data.project;
-    const personnel = item.data.project_personnel;
     const descriptionFrench = item.data.project_description_french || [];
     const descriptionEnglish = item.data.project_description_english || [];
     const description = language === 'french' ?
@@ -26,19 +37,17 @@ export default function Projects() {
     const hasName = Boolean(name.length !== 0);
     const hasImage = Boolean(imageURL !== null);
     const hasDescription = Boolean(description.length !== 0);
-    const hasPersonnel = Boolean(personnel.length !== 0);
 
     return(
       <ProjectWrapper key={item.id}>
-        <Box flex={'1 0 40%'}>
+        <Box flex={'1 0 30%'}>
           {hasImage ? <img src={imageURL} width={'100%'}/> : ''}
         </Box>
         <Box pl={[0, 4]}>
           <PrismicRichText field={hasName ? name : ''} />
           <PrismicRichText field={hasDescription ? description : ''} />
-          <Text color={'primary'}>
-            <PrismicRichText field={hasPersonnel ? personnel : ''} />
-          </Text>
+          <Box my={5} color={'primary'}>{lineup}</Box>
+          
         </Box>
       </ProjectWrapper>
     );
--- src/pages/projects/styled.js
+++ src/pages/projects/styled.js
@@ -12,13 +12,14 @@ export const ProjectWrapper = styled(Box)(
       display: ['block', 'unset', 'flex', 'flex'],
       img: {
         border: `1px solid ${theme.colors.primaryMuted}`,
+        width: ['80%', '80%', '100%'],
       },
       h2: {
         margin: ['0'],
         fontSize: [theme.fontSizes[5], theme.fontSizes[5], theme.fontSizes[6]],
         color: [theme.colors.primary],
         position: ['absolute', 'absolute', 'initial'],
-        top: ['-45px', theme.space[5], 'unset'],
+        top: ['-55px', theme.space[5], 'unset'],
 
       },
       marginTop: [theme.space[7],theme.space[7],theme.space[5]],
